e268b034559ee2502abc374959eb3f47d3fdff88
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-08-14 16:07:54 +0900

    feat: Added controller

--- api/main.go
+++ api/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo"
+	"github.com/ono5/study-hacker/api/controller"
 	"github.com/ono5/study-hacker/api/server"
 )
 
 func main() {
 	server := server.NewServer()
-	server.Get("/", hello)
+	server.Get("/", controller.HelloHandler)
 	server.Run()
-}
-
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World")
 }
