a76f6bdb92a194d750ba1b075d34879ac678fde0
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-08-15 13:11:04 +0900

    Added post feature

--- frontend/src/components/CreateForm.vue
+++ frontend/src/components/CreateForm.vue
@@ -1,13 +1,39 @@
 <template>
-  <form action="#">
-    <input type="text" name="japanese" id="japanese" />
-    <input type="text" name="english" id="english" />
-    <input type="submit" />
-  </form>
+  <div>
+    <h3>Learning English</h3>
+    <label for="japanese">Japanese</label>
+    <textarea type="text" v-model="japanese" id="japanese" />
+    <label for="english">Enlish</label>
+    <textarea type="text" v-model="english" id="english" />
+    <br><br>
+    <button @click="create">Create</button>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data() {
+    return {
+      japanese: "",
+      english: ""
+    }
+  },
+  methods: {
+    create() {
+      axios.post("localhost:8080/create", {
+        "japanese": this.japanese,
+        "english": this.english
+      })
+      .then(response => {
+        console.log({response})
+      })
+      .catch(err => console.log({err}))
+      this.japanese = ''
+      this.english = ''
+    }
+  }
 }
 </script>
 
