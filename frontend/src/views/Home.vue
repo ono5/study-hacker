84a9e472c2617af80923c2ac1f0e1046e0792e11
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-08-15 12:53:07 +0900

    added axios

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <CreateForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import CreateForm from '@/components/CreateForm'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+    CreateForm
   }
 }
 </script>
