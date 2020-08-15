84854065d952b10eea2401b324ddf1a5f811373e
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-08-15 12:52:53 +0900

    Create Form

---
+++ frontend/src/components/CreateForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form action="#">
+    <input type="text" name="japanese" id="japanese" />
+    <input type="text" name="english" id="english" />
+    <input type="submit" />
+  </form>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
